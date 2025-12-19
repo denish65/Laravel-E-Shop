8893c4032b03e92ef0bd82aaa41ad6d24a3a7b8c
denish <denishakbari1@gmail.com>
2025-12-19 13:47:09 +0530

    product observer functions

--- app/Observers/ProductObserver.php
+++ app/Observers/ProductObserver.php
@@ -2,9 +2,39 @@
 
 namespace App\Observers;
 use App\Models\Product;
+use App\Models\LogModel;
 
 
 class ProductObserver
 {
-    //
+    public function created(Product $Product)
+    {
+        LogModel::create([
+            "user_id" => Auth::id(),
+            "Action" => "created",
+            "data" => $Product->toArray(),
+        ]);
+    }
+
+    public function updated(Product $Product)
+    {
+           LogModel::create([
+            "user_id" => Auth::id(),
+            "Action" => "created",
+            "data" => $Product->toArray(),
+        ]);
+    }
+
+      public function deleted(Product $Product)
+    {
+           LogModel::create([
+            "user_id" => Auth::id(),
+            "Action" => "deleted",
+            "data" => $Product->toArray(),
+        ]);
+    }
+
+
+
+
 }
