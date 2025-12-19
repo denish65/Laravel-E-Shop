7e0ce973de0489359a8a5ae34f8cf0ac7367a795
denish <denishakbari1@gmail.com>
2025-12-19 12:19:18 +0530

    log migration

--- database/migrations/2025_12_19_061935_create_log_table.php
+++ database/migrations/2025_12_19_061935_create_log_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('log', function (Blueprint $table) {
             $table->id();
+            $table->string('user_id')->nullable(); // Who performed the action
+            $table->string('action'); // create, update, delete
+            $table->json('data'); // Full data in JSON format
             $table->timestamps();
         });
     }
