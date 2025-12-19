38f4d3f5b7389052a773528711e3d4864b1f41a8
denish <denishakbari1@gmail.com>
2025-12-19 12:31:38 +0530

    observer dependency

--- app/Observers/LogObserver.php
+++ app/Observers/LogObserver.php
@@ -1,8 +1,15 @@
 <?php
 
 namespace App\Observers;
+use App\Models\LogModel;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\Cart;
+use App\Models\Brand;
+
 
 class LogObserver
 {
-    //
+    
 }
